ca8ad017ef08b44c4e3023f1d5c17870d6116e68
yemiesther <105824298+yemiesther@users.noreply.github.com>
2022-06-22 00:02:36 +0100

    first step

---
+++ node.js
@@ -0,0 +1,12 @@
+
+
+//name, height, and country
+let fname='Adepoju';
+let lname='Adeyemi';
+myName=(fname +" " +lname);
+console.log(myName);
+var height=1.58;
+height= height+'m';
+console.log(height);
+const country='Nigeria';
+console.log(country);
